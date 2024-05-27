582514c7c358f032fe6b493fd579ee2c588a0faa
Ashton Breer <165852746+Ashton46@users.noreply.github.com>
2024-05-27 12:38:45 -0700

    Update script.js

--- m09/script.js
+++ m09/script.js
@@ -1,1 +1,26 @@
+var clearButton = document.getElementById("clearButton");
+
+clearButton.addEventListener("click", function(event) {
+  var elements = document.getElementsByClassName("dot");
+  while (elements.length > 0) {
+    elements[0].parentNode.removeChild(elements[0]);
+  }
+  event.stopPropagation();
+});
+
+addEventListener("click", function(event) {
+  if (event.target.id !== "clearButton" && event.target.tagName !== "INPUT") {
+    var colorInput = document.getElementById("color");
+    var sizeInput = document.getElementById("size");
+
+    var dot = document.createElement("div");
+    dot.className = "dot";
+    var size = parseInt(sizeInput.value);
+    dot.style.width = dot.style.height = size + "px";
+    dot.style.backgroundColor = colorInput.value;
+    dot.style.left = (event.pageX - size / 2) + "px";
+    dot.style.top = (event.pageY - size / 2) + "px";
+    document.body.appendChild(dot);
+  }
+});
 
