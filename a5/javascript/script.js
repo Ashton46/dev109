3c7ea782b03dca95d609e62dde364c407e128018
Ashton Breer <165852746+Ashton46@users.noreply.github.com>
2024-06-03 17:58:22 -0700

    Update script.js

--- a5/javascript/script.js
+++ a5/javascript/script.js
@@ -6,7 +6,7 @@ var myImages = [
     "images/slideshow.jpg"
 ];
 
-var captionImages = ["Resume: We created a resume for the first artifact, learning how to use lists, breaks, headings, and other components in html", "Chessboard: We created a chessboard for the second artifact, learning how to use CSS to create the board.", "Rhombus: We created a rhombus for the third artifact, learning how to use functions in javascripts such as for loops and if statements.", "Form Validation: We created a form and validated it for the fourth artifact, learning document object model (DOM) and using if statements to validate teh form.", "Slideshow: We created a slideshow for the fifth artifact, learning more DOM elements as well as the EventListener to react to events as they occur."];
+var captionImages = ["Resume: We created a resume for the first artifact, learning how to use lists, breaks, headings, and other components in html", "Chessboard: We created a chessboard for the second artifact, learning how to use CSS to align everything up and create the board with a background.", "Rhombus: We created a rhombus for the third artifact, learning how to use functions in javascripts such as for loops and if statements.", "Form Validation: We created a form and validated it for the fourth artifact, learning document object model (DOM) and using if statements to validate teh form.", "Slideshow: We created a slideshow for the fifth artifact, learning more DOM elements as well as the EventListener to react to events as they occur."];
 var index = 0;
 var interval;
 
