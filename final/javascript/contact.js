9f13e196e4b22aaeaa1e539135d6357bc92b3b31
Ashton Breer <165852746+Ashton46@users.noreply.github.com>
2024-06-11 23:05:02 -0700

    Update contact.js

--- final/javascript/contact.js
+++ final/javascript/contact.js
@@ -1,1 +1,26 @@
+function validateForm() {
+    var name = document.forms["myContact"]["name"].value;
+    var email = document.forms["myContact"]["subject"].value;
+    var message = document.forms["myContact"]["message"].value;
+    var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
 
+    if (name == "") {
+        alert("Name must be filled out");
+        return false;
+    }
+
+    if (email == "") {
+        alert("Email must be filled out");
+        return false;
+    } else if (!emailPattern.test(email)) {
+        alert("Please enter a valid email address");
+        return false;
+    }
+
+    if (message == "") {
+        alert("Message must be filled out");
+        return false;
+    }
+
+    return true;
+}
