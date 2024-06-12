987ef61844599a92eb21bfe0516277bd49660220
Ashton Breer <165852746+Ashton46@users.noreply.github.com>
2024-06-12 01:31:36 -0700

    Update script.js

--- final/javascript/script.js
+++ final/javascript/script.js
@@ -1,1 +1,20 @@
 
+const audio = new Audio('audio/audio.mp3');
+
+
+const audioBtn = document.getElementById('audio-btn');
+
+
+function toggleAudio() {
+    if (audio.paused) {
+        audio.play();
+        audioBtn.textContent = 'Pause Audio';
+    } else {
+        audio.pause();
+        audioBtn.textContent = 'Play Audio';
+    }
+}
+
+
+audioBtn.addEventListener('click', toggleAudio);
+
